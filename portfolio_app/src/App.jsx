21333372d36570acc3b5e8ab97e5dc55fb8eadcb
Jonathan <jonathanholson@gmail.com>
2019-12-20 15:55:31 -0800

    Added Starter CSS, CSS Color Variables

--- portfolio_app/src/App.jsx
+++ portfolio_app/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import { useSpring, animated, config } from 'react-spring';
 
 function App() {
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: config.molasses,
+    duration: 10000,
+  });
   return (
     <div className="welcome valign-wrapper">
       <div className="row">
         <div className="col s12"></div>
         <div className="col s12 m4 l1"></div>
-        <div className="col s12 m4 l10">
-          <p>Jonathan Olson</p>
-        </div>
+        <animated.div style={props} className="col s12 m4 l10 name-container">
+          <p className="name">Jonathan Olson</p>
+        </animated.div>
         <div className="col s12 m4 l1"></div>
       </div>
     </div>
