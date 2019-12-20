424675331c7bb3558e2da9429e358011390de28d
Jonathan <jonathanholson@gmail.com>
2019-12-20 15:04:05 -0800

    Initialized Rendering Landing Page

---
+++ portfolio_app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="welcome valign-wrapper">
+      <div className="row">
+        <div className="col s12"></div>
+        <div className="col s12 m4 l1"></div>
+        <div className="col s12 m4 l10">
+          <p>Jonathan Olson</p>
+        </div>
+        <div className="col s12 m4 l1"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
